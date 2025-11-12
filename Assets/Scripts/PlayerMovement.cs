9484745ee1eb182f96e38f43e18c6e9dbf600654
IvanBabichevv <tobbi.totti@gmail.com>
2025-11-12 13:40:17 +0300

    Main level design

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-
 public class PlayerMovement : MonoBehaviour
 {
-    public static PlayerMovement Instance {get; private set;}
-    
+    public static PlayerMovement Instance { get; private set; }
+
     [SerializeField] private CharacterController conroller;
     [SerializeField] private Transform cam;
 
@@ -17,79 +16,93 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float turnSmoothTime = 0.1f;
     [SerializeField] private float jumpHeight = 0.5f;
     [SerializeField] private float gravity = -9.8f;
-    
+
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private float IncreaseCooldown = 0.1f;
-    public Animator animator;
-    
+
+    [SerializeField] private Animator animator;  
 
     public float GetJumpPower() => jumpHeight;
-    
-    
+
     Vector3 velocity;
     bool isGrounded;
-    
+
     float turnSmoothVelocity;
     private float nextIncreaseCooldown = 0;
+
+    private Vector3 lastPosition;
+    private float moveMinimum = 0.001f; 
 
     private void Awake()
     {
         Instance = this;
     }
 
-    public void Start()
+    private void Start()
     {
-        animator = GetComponentInChildren<Animator>();
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
+
+        lastPosition = transform.position;
     }
 
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        
+
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
+
         Vector3 move = new Vector3(x, 0, z);
 
         if (move.magnitude > 1f)
             move.Normalize();
 
-       
         if (move.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            
+
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             conroller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
             print("Jump with height: " + jumpHeight.ToString("F2"));
         }
-        
+
         if (Input.GetMouseButton(0) && Time.time >= nextIncreaseCooldown)
         {
-            
+
         }
 
-        
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -10f;
-        } 
+            velocity.y = -2f;
+        }
         else
         {
             velocity.y += gravity * Time.deltaTime;
         }
-        
+
         conroller.Move(velocity * Time.deltaTime);
         
+        float distance = Vector3.Distance(transform.position, lastPosition);
+        bool isMoving = distance > moveMinimum;
+
+       
+        if (animator != null)
+        {
+            animator.SetBool("isMoving", isMoving);
+            animator.SetBool("isJumping", !isGrounded); 
+        }
+
+        lastPosition = transform.position; 
     }
 
     public void IncreaseJumpPower()
@@ -99,22 +112,5 @@ public class PlayerMovement : MonoBehaviour
         jumpHeight = Mathf.Clamp(jumpHeight, 0.001f, 50f);
         nextIncreaseCooldown = Time.time + IncreaseCooldown;
         PointsManager.Instance.ScoreChangedInvoke();
-        //print("Jump power increased: "  + jumpHeight.ToString("F2"));
     }
-
-    public void MoveInput(float horizontal, float vertical)
-    {
-       
-        
-        
-    }
-    
-    //public bool IsMoving
-    //{
-        //get
-        //{
-            //float distance = Vector3.Distance(transform.position, lastPosition);
-            //return (distance > moveMinimum);
-        //}
-   // }
 }
